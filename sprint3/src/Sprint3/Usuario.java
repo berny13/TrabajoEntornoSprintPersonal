e6096dde27a828db63f78d0db6f4cfa223cf41dd
berny13 <57494751+berny13@users.noreply.github.com>
2020-05-14 19:03:34 +0200

    se corrige codigo

--- sprint3/src/Sprint3/Usuario.java
+++ sprint3/src/Sprint3/Usuario.java
@@ -2,18 +2,19 @@ package Sprint3;
 
 import java.util.ArrayList;
 
-public class Entrar extends Usuario {
+public class Usuario  {
 	
 	ArrayList<Usuario> entrar;
 	
-	public Entrar(String username, String email, String password) {
+	public Usuario(String username, String email, String password) {
 		super(username, email, password);
 		
 	}
-	public void Entrar(String username, String password) {
+	public void Usuario(String username, String password) {
 		
 		}
 	public void beber() {
 		equals(getUsername());
 	}
 }
+
